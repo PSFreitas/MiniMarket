62adcb3950c4269a77690d420b7e2036067e2c6f
PSFreitas <pssfjr@gmail.com>
2020-04-16 11:49:44 -0400

    dimp:implement RecyclerView

--- app/src/main/java/com/minimarket/activity/MainActivity.kt
+++ app/src/main/java/com/minimarket/activity/MainActivity.kt
@@ -1,23 +1,49 @@
 package com.minimarket.activity
 
+
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxItemDecoration
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import com.minimarket.R
 import com.minimarket.network.service.ProductResponse
 import com.minimarket.network.service.ProductService
+import com.minimarket.ui.adapter.ProductAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var productAdapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getAllProducts()
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val flexBox = FlexboxLayoutManager(this)
+        flexBox.flexDirection = FlexDirection.ROW
+        flexBox.justifyContent = JustifyContent.SPACE_AROUND
+
+        val itemDecoration = FlexboxItemDecoration(this)
+        itemDecoration.setDrawable(ContextCompat.getDrawable(this, R.drawable.product_decorator))
+        itemDecoration.setOrientation(FlexboxItemDecoration.HORIZONTAL)
+
+
+        recyclerView_products.addItemDecoration(itemDecoration)
+        recyclerView_products.layoutManager = flexBox
+
     }
 
     private fun getAllProducts() {
@@ -34,8 +60,9 @@ class MainActivity : AppCompatActivity() {
                 if (response.isSuccessful) {
                     val products = response.body()?.products
                     if (products != null) {
-                        for (product in products)
-                            Log.i("PRODUTO", product.name)
+                        productAdapter = ProductAdapter(products, this@MainActivity)
+                        recyclerView_products.adapter = productAdapter
+                        productAdapter.notifyDataSetChanged()
                     }
 
                 }
