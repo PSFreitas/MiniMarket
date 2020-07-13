f7c933ba8c7383de6bfad22327566e60429eff50
Paulo <paulo.junior@ITBAM.ORG.BR>
2020-07-13 16:05:37 -0400

    test: implement ProductList request object validation

--- data/src/main/java/com/example/data/network/service/ProductService.kt
+++ data/src/main/java/com/example/data/network/service/ProductService.kt
@@ -1,6 +1,7 @@
 package com.example.data.network.service
 
 import com.example.data.network.response.ProductResponse
+import okhttp3.HttpUrl
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
@@ -10,18 +11,19 @@ import retrofit2.http.GET
 interface ProductService {
 
     @GET("59b6a65a0f0000e90471257d")
-    fun getAllProducts(): Response<ProductResponse>
+    suspend fun getAllProducts(): Response<ProductResponse>
 
     companion object Factory {
         private const val BASE_URL = "http:www.mocky.io/v2/"
 
-        fun create(): ProductService {
+        fun create(baseUrl: HttpUrl = HttpUrl.parse(BASE_URL)!!): ProductService {
             return Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(BASE_URL)
+                .baseUrl(baseUrl)
                 .build()
                 .create(ProductService::class.java)
         }
+
 
     }
 }
