97fe2b3330d2f5277c7ec93fbb66727a1a7317b1
Paulo <pssfjr@gmail.com>
2020-08-08 11:19:40 -0400

    feat: add CartItemCount formatter

--- app/src/main/java/com/minimarket/utils/BindingAdapters.kt
+++ app/src/main/java/com/minimarket/utils/BindingAdapters.kt
@@ -1,11 +1,7 @@
 package com.minimarket.utils
 
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import com.minimarket.R
-import com.squareup.picasso.Callback
-import com.squareup.picasso.Picasso
 import java.math.BigDecimal
 import java.text.NumberFormat
 import java.util.*
@@ -16,30 +12,22 @@ class BindingAdapters {
         @BindingAdapter("formatPrice")
         fun formatPrice(textView: TextView, price: String) {
             val bigDecimal = BigDecimal(price)
-            val formattedPrice = NumberFormat.getCurrencyInstance(Locale("pt", "BR")).format(bigDecimal)
-            textView.text = formattedPrice.replace("R$","R$ ")
+            val formattedPrice =
+                NumberFormat.getCurrencyInstance(Locale("pt", "BR")).format(bigDecimal)
+            textView.text = formattedPrice.replace("R$", "R$ ")
 
         }
+
 
         @JvmStatic
-        @BindingAdapter("setseImage")
-        fun setImage(imageView: ImageView, path: String) {
-            if (path.isNotBlank() && path.isNotEmpty())
-                Picasso
-                    .get()
-                    .load(path)
-                    .placeholder(android.R.color.white)
-                    .error(R.color.cardview_dark_background)
-                    .into(imageView, object : Callback {
-                        override fun onSuccess() {
-                            TODO("Not yet implemented")
-                        }
-
-                        override fun onError(e: Exception?) {
-                            TODO("Not yet implemented")
-                        }
-
-                    })
+        @BindingAdapter("formatCartItemCount")
+        fun formatCartItemCount(textView: TextView, itemCount: Int) {
+            if (itemCount > 99) {
+                textView.text = "99+"
+            } else {
+                textView.text = itemCount.toString()
+            }
         }
+
     }
 }
